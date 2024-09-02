56c1df9bc12778b0e869e3c5810deb85662a0fec
L-E-RAN <eliran.ashwal@gmail.com>
2024-09-03 00:01:53 +0300

    Delete all files from GitHub repository

--- delete_all_files_from_github.sh
+++ delete_all_files_from_github.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+# מחיקת כל הקבצים כולל הסקריפט מהמאגר ב-GitHub
+git rm -r --cached *
 
-# נווט לתיקייה שבה נמצאים קבצי האתר שלך
-cd /C/Users/Eliran\ Ashwal/Desktop/InnovativeBroomShop
+# מחיקת הסקריפט מהמחשב המקומי אחרי שהוא רץ
+rm delete_all_files_from_github.sh
 
-# הסרת כל הקבצים מהמאגר ב-GitHub (ללא מחיקתם מהמחשב המקומי)
-git rm -r *
-
-# יצירת commit למחיקת הקבצים
-git commit -m "Delete all files from GitHub repository"
+# יצירת commit למחיקת הקבצים מהמאגר
+git commit -m "Delete all files from GitHub repository, including script"
 
 # דחיפת השינויים למחיקת כל התוכן ב-branch 'main' ב-GitHub
 git push origin main
