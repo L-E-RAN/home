b32822d09e4165b183f5f53fc68eb79753833444
L-E-RAN <eliran.ashwal@gmail.com>
2024-09-03 00:37:34 +0300

    Upload fresh content from local directory

--- upload_clean_to_github.sh
+++ upload_clean_to_github.sh
@@ -3,13 +3,13 @@
 # נווט לתיקייה שבה נמצאים קבצי האתר שלך
 cd /C/Users/Eliran\ Ashwal/Desktop/InnovativeBroomShop
 
-# מחיקת כל הקבצים מהמאגר ב-GitHub (ללא מחיקתם מהמחשב המקומי)
+# הסרת כל הקבצים מהמאגר ב-GitHub (ללא מחיקתם מהמחשב המקומי)
 git rm -r --cached *
 
-# יצירת commit למחיקת הקבצים מהמאגר ב-GitHub
-git commit -m "Delete all files from GitHub repository"
+# יצירת commit למחיקת הקבצים מהמאגר
+git commit -m "Remove all files from GitHub repository"
 
-# דחיפת השינויים למחיקת כל התוכן ב-branch 'main' ב-GitHub
+# דחיפת השינויים ל-branch 'main' ב-GitHub
 git push origin main
 
 # הוספת כל הקבצים המקומיים שוב למעקב
